7772f358052b8943d20c6b346f1987e075f58a61
Brian C. Lindner <brian@bclindner.com>
2018-12-12 17:14:37 -0500

    Game now only ticks when moved

--- main.go
+++ main.go
@@ -17,22 +17,29 @@ func main() {
 Loop:
 	for {
 		score := 0
+		moved := false
 		grid := GameGrid{}
 		switch ev := termbox.PollEvent(); ev.Type {
 		case termbox.EventKey:
 			switch ev.Key {
 			case termbox.KeyArrowUp:
 				grid, score = game.Grid.MergeUp()
+				moved = true
 			case termbox.KeyArrowDown:
 				grid, score = game.Grid.MergeDown()
+				moved = true
 			case termbox.KeyArrowLeft:
 				grid, score = game.Grid.MergeLeft()
+				moved = true
 			case termbox.KeyArrowRight:
 				grid, score = game.Grid.MergeRight()
+				moved = true
 			case termbox.KeyCtrlC:
 				break Loop
 			}
 		}
-		game.Tick(grid, score)
+		if moved {
+			game.Tick(grid, score)
+		}
 	}
 }
